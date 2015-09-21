e5cd87f7293dd5afabc9eb8d4da40d5689442db7
Bence Dányi <bence@danyi.me>
2015-09-22 01:08:42 +0200

    Add message class to message dom nodes

--- src/client/components/message.js
+++ src/client/components/message.js
@@ -3,6 +3,6 @@ import React from 'react';
 export default class Message extends React.Component {
   displayName: 'Message'
   render() {
-    return <div>{this.props.text}</div>
+    return <div className='message'>{this.props.text}</div>
   }
 }
