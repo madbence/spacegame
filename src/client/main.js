a042ac4729a760a2075f7eef4dba38012217e9a0
Bence Dányi <bence@danyi.me>
2015-09-29 01:43:31 +0200

    feat(client): add basic simulation control

--- src/client/main.js
+++ src/client/main.js
@@ -10,3 +10,86 @@ React.render(
   </Provider>,
   document.getElementById('mount')
 );
+
+const tick = () => {
+  store.dispatch({ type: 'TICK' });
+  requestAnimationFrame(tick);
+};
+
+const ctx = document.getElementById('canvas').getContext('2d');
+function render() {
+  const state = store.getState();
+  ctx.save();
+  ctx.clearRect(0, 0, 500, 500);
+  ctx.translate(250.5, 250.5);
+  for (const ship of state.ships) {
+    ctx.translate(ship.pos.x, ship.pos.y);
+    ctx.rotate(ship.rot);
+    ctx.fillRect(-5, -10, 10, 20);
+    if (ship.thrust) {
+      ctx.save();
+      ctx.fillStyle = 'red';
+      ctx.fillRect(-2.5, -20, 5, 10);
+      ctx.restore();
+    }
+    if (ship.rotThrust < 0) {
+      ctx.save();
+      ctx.fillStyle = 'red';
+      ctx.fillRect(2.5, -7.5, 5, 5);
+      ctx.fillRect(-7.5, 2.5, 5, 5);
+      ctx.restore();
+    }
+    if (ship.rotThrust > 0) {
+      ctx.save();
+      ctx.fillStyle = 'red';
+      ctx.fillRect(-7.5, -7.5, 5, 5);
+      ctx.fillRect(2.5, 2.5, 5, 5);
+      ctx.restore();
+    }
+  }
+  ctx.restore();
+}
+
+store.subscribe(render);
+
+document.addEventListener('keydown', e => {
+  switch (e.keyCode) {
+    case 87:
+    store.dispatch({
+      type: 'ACCELERATE',
+      index: 0,
+      state: true,
+    }); break;
+    case 65:
+    store.dispatch({
+      type: 'ROTATE',
+      index: 0,
+      dir: -0.001,
+    }); break;
+    case 68:
+    store.dispatch({
+      type: 'ROTATE',
+      index: 0,
+      dir: 0.001,
+    }); break;
+  }
+});
+document.addEventListener('keyup', e => {
+  switch (e.keyCode) {
+    case 87:
+    store.dispatch({
+      type: 'ACCELERATE',
+      index: 0,
+      state: false,
+    }); break;
+    case 65:
+    case 68:
+    store.dispatch({
+      type: 'ROTATE',
+      index: 0,
+      dir: 0,
+    }); break;
+  }
+});
+
+tick();
