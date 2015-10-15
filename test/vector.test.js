5e5f10cdd5d1e6dd5bdc897b6b98846f0d0d26e3
Bence Dányi <bence@danyi.me>
2015-10-15 17:41:43 +0200

    test(vector): add test for vector#add

---
+++ test/vector.test.js
@@ -0,0 +1,28 @@
+import test from 'ava';
+import * as v from '../src/common/util/vector';
+
+test('vector#add', t => {
+  const a = { x: 1, y: 0 };
+  const b = { x: 1, y: 2 };
+  t.same(v.add(a, b), { x: 2, y: 2 });
+  t.is(a.x, 1);
+  t.is(a.y, 0);
+  t.is(b.x, 1);
+  t.is(b.y, 2);
+
+  [
+    [[0, 0], [0, 0], [0, 0]],
+    [[0, 0], [1, 1], [1, 1]],
+    [[1, 1], [0, 0], [1, 1]],
+    [[1, 1], [1, 1], [2, 2]],
+  ].forEach(([[x1, y1], [x2, y2], [x3, y3]]) => {
+    t.same(v.add({
+      x: x1, y: y1
+    }, {
+      x: x2, y: y2
+    }), {
+      x: x3, y: y3
+    });
+  });
+  t.end();
+});
