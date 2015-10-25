b0adac3001944a67209f4ea96fa2250b3735f8b6
Bence Dányi <bence@danyi.me>
2015-10-25 19:28:27 +0100

    refactor(websocket): refactor client-side websocket connection

--- src/client/middlewares/websocket.js
+++ src/client/middlewares/websocket.js
@@ -1,27 +1,53 @@
 import WS from 'ws';
 
-let client;
+class Client {
+  constructor(store) {
+    this.queue = [];
+    this.store = store;
+    this.connect();
+  }
 
-function send(type, payload, meta) {
-  const message = JSON.stringify({
-    type,
-    payload,
-    meta,
-  });
+  connect() {
+    const socket = this.socket = new WS('ws://localhost:3000');
+    socket.onmessage = this._read.bind(this);
+    socket.onopen = this._drain.bind(this);
+    socket.onclose = this._close.bind(this);
+  }
 
-  client.send(message);
+  send(action) {
+    const message = JSON.stringify(action);
+    if (!this.socket || this.socket.readyState != 1) {
+      this.queue.push(message);
+      return;
+    }
+    this.socket.send(message);
+  }
+
+  _read(event) {
+    const action = JSON.parse(event.data);
+    this.store.dispatch(action);
+  }
+
+  _drain() {
+    for (const message of this.queue) {
+      this.socket.send(message);
+    }
+    this.queue = [];
+  }
+
+  _close() {
+    setTimeout(this.connect.bind(this), 1000);
+  }
 }
+
+let client;
 
 export default store => next => action => {
   if (action.meta && action.meta.pending) {
     if (!client) {
-      client = WS('ws://localhost:3000');
-      client.onmessage = event => {
-        const action = JSON.parse(event.data);
-        store.dispatch(action);
-      };
+      client = new Client(store);
     }
-    return send(action.type, action.payload, action.meta);
+    return client.send(action);
   }
   return next(action);
 };
