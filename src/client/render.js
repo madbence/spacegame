2b1fecb259813dcc2b4d9239b690809924bf65ec
Bence Dányi <bence@danyi.me>
2015-10-28 18:49:33 +0100

    feat(render): visualize future prediction
    not really useful, but it looks cool. since the simulation is deterministic,
    we can forward time at will, and render the state at the given time

--- src/client/render.js
+++ src/client/render.js
@@ -28,41 +28,56 @@ export default function render(offset, store) {
     ctx.translate(250.5, 250.5);
     ctx.scale(1, -1);
 
-    // for each ship
-    for (const ship of state.ships) {
-      // draw hull
-      ctx.save();
-      ctx.translate(ship.position.x, ship.position.y);
-      ctx.rotate(ship.orientation);
-      ctx.fillRect(-5, -10, 10, 20);
-      ctx.beginPath();
-      ctx.moveTo(-5, -10);
-      ctx.lineTo(-5, 10);
-      ctx.lineTo(0, 15);
-      ctx.lineTo(5, 10);
-      ctx.lineTo(5, -10);
-      ctx.fill();
-      for (const thruster of ship.thrusters) {
-        if (thruster.strength === 0) {
-          continue;
-        }
+    function renderState(state) {
+
+      // for each ship
+      for (const ship of state.ships) {
+        // draw hull
         ctx.save();
-        ctx.fillStyle = 'red';
-        ctx.translate(thruster.position.x, thruster.position.y);
-        ctx.rotate(thruster.orientation);
-        ctx.fillRect(-2, 0, 4, Math.log(10000 * thruster.strength) * -2);
+        ctx.translate(ship.position.x, ship.position.y);
+        ctx.rotate(ship.orientation);
+        ctx.fillRect(-5, -10, 10, 20);
+        ctx.beginPath();
+        ctx.moveTo(-5, -10);
+        ctx.lineTo(-5, 10);
+        ctx.lineTo(0, 15);
+        ctx.lineTo(5, 10);
+        ctx.lineTo(5, -10);
+        ctx.fill();
+        for (const thruster of ship.thrusters) {
+          if (thruster.strength === 0) {
+            continue;
+          }
+          ctx.save();
+          ctx.fillStyle = 'red';
+          ctx.translate(thruster.position.x, thruster.position.y);
+          ctx.rotate(thruster.orientation);
+          ctx.fillRect(-2, 0, 4, Math.log(10000 * thruster.strength) * -2);
+          ctx.restore();
+        }
         ctx.restore();
       }
-      ctx.restore();
+
+      for (const projectile of state.projectiles) {
+        ctx.save();
+        ctx.translate(projectile.position.x, projectile.position.y);
+        ctx.rotate(projectile.orientation);
+        ctx.fillStyle = 'green';
+        ctx.fillRect(-2.5, -10, 5, 20);
+        ctx.restore();
+      }
     }
 
-    for (const projectile of state.projectiles) {
-      ctx.save();
-      ctx.translate(projectile.position.x, projectile.position.y);
-      ctx.rotate(projectile.orientation);
-      ctx.fillStyle = 'green';
-      ctx.fillRect(-2.5, -10, 5, 20);
-      ctx.restore();
+    renderState(state);
+    for (let i = 0; i < 10; i++) {
+      ctx.globalAlpha = 0.2 / i;
+      state = simulate(state, {
+        type: 'NOOP',
+        payload: {
+          time: Date.now() - offset + i * 500,
+        },
+      });
+      renderState(state);
     }
     ctx.restore();
   }
