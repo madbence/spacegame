142f2ad34f317456c68ac71b10c12b6097ffb495
Bence Dányi <bence@danyi.me>
2015-11-24 19:14:23 +0100

    refactor: move ship rendering to separate file

--- src/client/render/index.js
+++ src/client/render/index.js
@@ -3,6 +3,7 @@ import subscribe from '../lib/keypress';
 import store from '../store';
 
 import renderLoadScreen from './load';
+import renderShip from './ship';
 
 import {
   length,
@@ -130,46 +131,8 @@ class GameClient {
     ctx.stroke();
     ctx.restore();
 
-    // for each ship
     for (const ship of game.ships) {
-      // draw hull
-      ctx.save();
-      ctx.translate(ship.position.x, ship.position.y);
-      ctx.save();
-      ctx.rotate(viewport.orientation);
-        ctx.save();
-        ctx.fillStyle = 'green';
-        ctx.fillRect(-20, 20, ship.hull * 0.4, 3);
-        ctx.fillStyle = 'red';
-        ctx.fillRect(-20 + ship.hull * 0.4, 20, (100 - ship.hull) * 0.4, 3);
-        ctx.restore();
-        ctx.save();
-        ctx.rotate(Math.PI);
-        ctx.scale(-1, 1);
-        ctx.fillText(ship.name, -20, -25);
-        ctx.restore();
-      ctx.restore();
-      ctx.rotate(ship.orientation);
-      ctx.fillRect(-5, -10, 10, 20);
-      ctx.beginPath();
-      ctx.moveTo(-5, -10);
-      ctx.lineTo(-5, 10);
-      ctx.lineTo(0, 15);
-      ctx.lineTo(5, 10);
-      ctx.lineTo(5, -10);
-      ctx.fill();
-      for (const thruster of ship.thrusters) {
-        if (thruster.strength === 0) {
-          continue;
-        }
-        ctx.save();
-        ctx.fillStyle = 'rgba(255, 0, 0, 0.8)';
-        ctx.translate(thruster.position.x, thruster.position.y);
-        ctx.rotate(thruster.orientation);
-        ctx.fillRect(-2, 0, 4, Math.log(200 * thruster.strength) * -2);
-        ctx.restore();
-      }
-      ctx.restore();
+      renderShip(ctx, ship, viewport);
     }
 
     for (const projectile of game.projectiles) {
---
+++ src/client/render/ship.js
@@ -0,0 +1,39 @@
+export default (ctx, ship, viewport) => {
+  ctx.save();
+  ctx.translate(ship.position.x, ship.position.y);
+  ctx.save();
+  ctx.rotate(viewport.orientation);
+    ctx.save();
+    ctx.fillStyle = 'green';
+    ctx.fillRect(-20, 20, ship.hull * 0.4, 3);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(-20 + ship.hull * 0.4, 20, (100 - ship.hull) * 0.4, 3);
+    ctx.restore();
+    ctx.save();
+    ctx.rotate(Math.PI);
+    ctx.scale(-1, 1);
+    ctx.fillText(ship.name, -20, -25);
+    ctx.restore();
+  ctx.restore();
+  ctx.rotate(ship.orientation);
+  ctx.fillRect(-5, -10, 10, 20);
+  ctx.beginPath();
+  ctx.moveTo(-5, -10);
+  ctx.lineTo(-5, 10);
+  ctx.lineTo(0, 15);
+  ctx.lineTo(5, 10);
+  ctx.lineTo(5, -10);
+  ctx.fill();
+  for (const thruster of ship.thrusters) {
+    if (thruster.strength === 0) {
+      continue;
+    }
+    ctx.save();
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.8)';
+    ctx.translate(thruster.position.x, thruster.position.y);
+    ctx.rotate(thruster.orientation);
+    ctx.fillRect(-2, 0, 4, Math.log(200 * thruster.strength) * -2);
+    ctx.restore();
+  }
+  ctx.restore();
+};
