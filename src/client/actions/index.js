567a560379ae9c49ffcfbdf795371527cc303da1
Peter Garamvolgyi <th307q@gmail.com>
2015-12-16 10:06:10 +0100

    refactor: add action creators
    these serve as factory functions for actions. note: the remote dispatch logic (setting meta.pending=true) is hidden in these when possible.

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,3 +1,25 @@
+// local actions
 export const CLIENT_CONNECT = 'client-connect';
 export const CLIENT_DISCONNECT = 'client-disconnect';
 export const NAVIGATE = 'navigate';
+
+// action creators
+export const clientConnect = () => ({
+  type: CLIENT_CONNECT,
+  payload: {},
+  meta: {},
+});
+
+export const clientDisconnect = () => ({
+  type: CLIENT_DISCONNECT,
+  payload: {},
+  meta: {},
+});
+
+export const navigate = (route) => ({
+  type: NAVIGATE,
+  payload: {
+    route,
+  },
+  meta: {},
+});
--- src/shared/actions/index.js
+++ src/shared/actions/index.js
@@ -1,7 +1,78 @@
+// remote actions initiated by client
 export const FIRE_WEAPON = 'fire-weapon';
 export const SET_THRUST = 'set-thrust';
 export const JOIN_GAME = 'join-game';
+
+// actions initiated by server
 export const CLIENT_INIT = 'client-init';
 export const JOIN_PLAYER = 'join-player';
 export const SYNC_GAME = 'sync-game';
 export const HEARTBEAT = 'beat';
+
+// collection for validation
+export const validGameActions = [
+  FIRE_WEAPON,
+  SET_THRUST,
+  JOIN_PLAYER,
+  SYNC_GAME,
+  HEARTBEAT,
+];
+
+// action creators
+export const fireWeapon = () => ({
+  type: FIRE_WEAPON,
+  payload: {},
+  meta: {
+    pending: true,
+  },
+});
+
+export const setThrust = (thrusterIndex, strength) => ({
+  type: SET_THRUST,
+  payload: {
+    thrusterIndex,
+    strength,
+  },
+  meta: {
+    pending: true,
+  },
+});
+
+export const joinGame = (name) => ({
+  type: JOIN_GAME,
+  payload: {
+    name,
+  },
+  meta: {
+    pending: true,
+  },
+});
+
+export const clientInit = (id) => ({
+  type: CLIENT_INIT,
+  payload: {
+    id,
+  },
+  meta: {},
+});
+
+export const joinPlayer = (clientid, name) => ({
+  type: JOIN_PLAYER,
+  payload: {
+    client: clientid,
+    name,
+  },
+  meta: {},
+});
+
+export const syncGame = (gamestate) => ({
+  type: SYNC_GAME,
+  payload: gamestate,
+  meta: {},
+});
+
+export const heartbeat = () => ({
+  type: HEARTBEAT,
+  payload: {},
+  meta: {},
+});
