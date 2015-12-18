88f6d90b733543fc7427c02f432e0ef2957cd772
Bence Dányi <bence@danyi.me>
2015-12-18 23:46:53 +0100

    test: rewrite game tests

--- test/game.test.js
+++ test/game.test.js
@@ -1,53 +1,49 @@
 import test from 'ava';
-import game from '../src/shared/game';
-import { noop } from '../src/shared/actions';
+import { advanceShip } from '../src/shared/game/physics';
+
+const nv = { x: 0, y: 0 };
 
 test('stationary ship stays stationary', t => {
   const initial = {
-    time: 0,
-    step: 1,
-    ships: [{
-      position: { x: 0, y: 0 },
-      velocity: { x: 0, y: 0 },
-      orientation: 0,
-      rotation: 0,
-      thrusters: [],
-      hull: 1,
-    }],
-    projectiles: [],
-    explosions: [],
+    position: nv,
+    velocity: nv,
+    rotation: 0,
+    orientation: 0,
+    thrusters: [],
   };
-  let next = game(initial, noop(1)).ships;
 
-  t.same(next[0].position, { x: 0, y: 0 });
-  t.same(next[0].velocity, { x: 0, y: 0 });
-  t.same(next[0].rotation, 0);
-  t.same(next[0].orientation, 0);
+  let next = advanceShip(initial, 1);
+
+  t.same(next.position, nv);
+  t.same(next.velocity, nv);
+  t.same(next.rotation, 0);
+  t.same(next.orientation, 0);
 
   t.end();
 });
 
-test('moving ship does not lose energy', t => {
-  const initial = {
-    time: 0,
-    step: 1000,
-    ships: [{
-      position: { x: 0, y: 0 },
-      velocity: { x: 1, y: 1 },
+[
+  [0, 1],
+  [1, 0],
+  [1, 1],
+].forEach(([x, y]) => {
+  test(`ship moves to the right direction (${x}, ${y})`, t => {
+    const initial = {
+      position: nv,
+      velocity: { x, y },
+      rotation: 0,
       orientation: 0,
-      rotation: 1,
       thrusters: [],
-      hull: 1,
-    }],
-    projectiles: [],
-    explosions: [],
-  };
-  let next = game(initial, noop(1)).ships;
+    };
 
-  t.same(next[0].position, { x: 1, y: 1 });
-  t.same(next[0].velocity, { x: 1, y: 1 });
-  t.same(next[0].rotation, 1);
-  t.same(next[0].orientation, 1);
+    let next = advanceShip(initial, 1);
+    t.same(next.position, { x, y });
+    t.same(next.velocity, { x, y });
 
-  t.end();
+    next = advanceShip(next, 1);
+    t.same(next.position, { x: 2 * x, y: 2 * y });
+    t.same(next.velocity, { x, y });
+
+    t.end();
+  });
 });
